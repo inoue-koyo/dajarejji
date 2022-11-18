86f6059c1b4570d08a1c63abfd89c6727a115f40
inoue-koyo <inoue.koyo.775@gmail.com>
2022-11-18 18:11:23 +0900

    update

--- bert_sentence_accuracy.py
+++ bert_sentence_accuracy.py
@@ -1,5 +1,5 @@
 from transformers import BertJapaneseTokenizer, BertForMaskedLM, pipeline, BertConfig
-import torch #Bertを使うのに必要？ 
+import torch #Bertを使うのに必要
 import re
 import numpy as np
 
@@ -7,6 +7,8 @@ import numpy as np
 def bert(input):
     # input='「イワシ・タイ・ナマズ」って言わしたいな、まず。'
     # input2='給料は玉ねぎのことです。'
+
+    torch._C._cuda_emptyCache()
 
     pretrained = 'cl-tohoku/bert-base-japanese-whole-word-masking'#事前学習済みモデルの選択
     tokenizer = BertJapaneseTokenizer.from_pretrained(pretrained) #分析器の事前学習モデルの選択
